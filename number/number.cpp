2548fb4c26881627f77eabd840da0afb799581a5
zemljanichka <belkazarina@mail.ru>
2022-10-03 10:24:13 +0300

    homework 2

--- number/number.cpp
+++ number/number.cpp
@@ -1,1 +1,373 @@
 #include "number.h"
+
+using namespace std;
+
+number_t::number_t() : x(0) {}
+
+
+number_t::number_t(int64_t o_x) : x(o_x){}
+
+number_t::number_t(double o_x) : x(o_x) {}
+
+
+number_t::number_t(int o_x) : x(o_x) {}
+
+number_t::number_t(const number_t &o_number_t) : x(o_number_t.x) {}
+
+number_t::operator int64_t() const
+{
+  return x;
+}
+
+number_t::operator int() const
+{
+  return int(x);
+}
+
+number_t::operator float() const
+{
+  return float(x);
+}
+
+number_t::operator bool() const
+{
+  return bool(x);
+}
+
+
+number_t::operator std::string() const
+{
+  return to_string(int64_t(x));
+}
+
+number_t& number_t::operator=(const number_t &o_x) = default;
+number_t& number_t::operator=(const int64_t &o_x) {
+  x = o_x;
+  return *this;
+}
+
+
+number_t operator+(const int64_t &val, number_t &val2){
+  number_t res;
+  res.x = val + val2.x;
+  return res;
+}
+
+
+number_t operator+(number_t &val2, const int64_t &val){
+  number_t res;
+  res.x = val + val2.x;
+  return res;
+}
+
+
+bool number_t::operator==(const number_t &val) const {
+  return this->x == val.x;
+}
+bool number_t::operator==(const int64_t &val) const {
+  return this->x == static_cast <long long>(val);
+}
+bool number_t::operator!=(const number_t &val) const {
+  return this->x != val.x;
+}
+bool number_t::operator!=(const int64_t &val) const {
+  return this->x != static_cast <long long>(val);
+}
+bool number_t::operator>(const number_t &val) const {
+  return this->x > val.x;
+}
+bool number_t::operator<(const number_t &val) const {
+  return this->x < val.x;
+}
+bool number_t::operator>=(const number_t &val) const {
+  return this->x >= val.x;
+}
+bool number_t::operator<=(const number_t &val) const {
+  return this->x <= val.x;
+}
+bool number_t::operator<=(const int64_t &val) const {
+  return this->x <= static_cast <long long>(val);
+}
+
+
+
+std::ostream &operator<<(std::ostream &os, const number_t& number) {
+  return (os << number.x);
+}
+
+std::istream &operator>>(std::istream &is, number_t& number) {
+  return is >> number.x;
+}
+
+
+
+bool operator==(const int64_t &val, const number_t &val2) {
+  return static_cast <long long>(val) == val2.x;
+}
+bool operator!=(const int64_t &val, const number_t &val2) {
+  return static_cast <long long>(val) != val2.x;
+}
+bool operator<(const int64_t &val, const number_t &val2) {
+  return static_cast <long long>(val) < val2.x;
+}
+bool number_t::operator<(const int64_t &val) const {
+  return this->x < static_cast <long long>(val);
+}
+bool number_t::operator>(const int64_t &val) const {
+  return this->x > static_cast <long long>(val);
+}
+bool operator>(const int64_t &val, const number_t &val2) {
+  return static_cast <long long>(val) > val2.x;
+}
+bool operator<=(const int64_t &val, const number_t &val2) {
+  return static_cast <long long>(val) <= val2.x;
+}
+bool operator>=(const int64_t &val, const number_t &val2) {
+  return static_cast <long long>(val) >= val2.x;
+}
+bool number_t::operator>=(const int64_t &val) const {
+  return this->x >= static_cast <long long>(val);
+}
+
+number_t& number_t::operator++(){
+  x++;
+  return *this;
+}
+
+number_t& number_t::operator--(){
+  x--;
+  return *this;
+}
+
+number_t number_t::operator++(int) {
+  number_t temp{*this};
+  ++(*this);
+  return temp;
+}
+
+number_t number_t::operator--(int) {
+  int64_t t = this->x;
+  number_t temp(t);
+  --(*this);
+  return temp;
+}
+
+number_t &number_t::operator+=(const int64_t &val2) {
+  x += val2;
+  return *this;
+}
+
+number_t operator+(const number_t &left, const number_t &right) {
+  number_t res;
+  res.x = left.x + right.x;
+  return res;
+}
+
+number_t operator-(const number_t &val, const int64_t &val2) {
+  number_t res;
+  res.x = val.x - val2;
+  return res;
+}
+
+number_t operator-(const int64_t &val, const number_t &val2) {
+  number_t res;
+  res.x = val - val2.x;
+  return res;
+}
+
+number_t number_t::operator-() const {
+  number_t temp;
+  temp.x = -this->x;
+  return temp;
+}
+
+number_t number_t::operator+() const {
+  return *this;
+}
+
+number_t &number_t::operator+=(const number_t &val2) {
+  x += val2.x;
+  return *this;
+}
+
+number_t& number_t::operator-=(const int64_t &val2){
+  x -= val2;
+  return *this;
+}
+
+number_t &number_t::operator-=(const number_t &val2) {
+  x -= val2.x;
+  return *this;
+}
+
+number_t operator-(const number_t &left, const number_t &right) {
+  number_t res;
+  res.x = left.x - right.x;
+  return res;
+}
+
+number_t operator*(const number_t &left, const int64_t &right) {
+  number_t res;
+  res.x = left.x * right;
+  return res;
+}
+
+number_t operator*(const int64_t &left, const number_t &right) {
+  number_t res;
+  res.x = left * right.x;
+  return res;
+}
+
+number_t operator*(const number_t &left, const number_t &right) {
+  number_t res;
+  res.x = left.x * right.x;
+  return res;
+}
+
+number_t number_t::operator*=(const int64_t &right) {
+  this->x *= right;
+  return *this;
+}
+
+number_t number_t::operator*=(const number_t &right) {
+  this->x *= right.x;
+  return *this;
+}
+
+number_t operator/(const number_t &left, const int64_t &right) {
+  number_t res;
+  res.x = left.x / right;
+  return res;
+}
+
+number_t operator/(const int64_t &left, const number_t &right) {
+  number_t res;
+  res.x = left / right.x;
+  return res;
+}
+
+number_t operator/(const number_t &left, const number_t &right) {
+  number_t res;
+  res.x = left.x / right.x;
+  return res;
+}
+
+number_t number_t::operator/=(const int64_t &right) {
+  this->x /= right;
+  return *this;
+}
+
+number_t number_t::operator/=(const number_t &right) {
+  this->x /= right.x;
+  return *this;
+}
+
+number_t operator%(number_t &left, const int64_t &right) {
+  return {int64_t(left.x) % right};
+}
+
+number_t operator%(const int64_t &left, number_t &right) {
+  return {left % int64_t(right.x)};
+}
+
+number_t operator%(const number_t &left, const number_t &right) {
+  return {int64_t(left.x) % int64_t(right.x)};
+}
+
+number_t number_t::operator%=(const int64_t &right) {
+  this->x = int64_t(this->x) % right;
+  return *this;
+}
+
+number_t number_t::operator%=(const number_t &right) {
+  this->x = int64_t(this->x) % int64_t(right.x);
+  return *this;
+}
+
+number_t operator^(number_t &left, const int64_t &right) {
+  return {int64_t(left.x) ^ right};
+}
+
+number_t operator^(const int64_t &left, number_t &right) {
+  return {left ^ int64_t(right.x)};
+}
+
+number_t operator^(const number_t &left, const number_t &right) {
+  return {int64_t(left.x) ^ int64_t(right.x)};
+}
+
+number_t number_t::operator^=(const int64_t &right) {
+  this->x = int64_t(this->x) ^right;
+  return *this;
+}
+
+number_t number_t::operator^=(const number_t &right) {
+  this->x = int64_t(this->x) ^int64_t(right.x);
+  return *this;
+}
+
+number_t operator&(number_t &left, const int64_t &right) {
+  return {int64_t(left.x) & right};
+}
+
+number_t operator&(const int64_t &left, number_t &right) {
+  return {left & int64_t(right.x)};
+}
+
+number_t operator&(const number_t &left, const number_t &right) {
+  return {int64_t(left.x) & int64_t(right.x)};
+}
+
+number_t number_t::operator&=(const int64_t &right) {
+  this->x = int64_t(this->x) & right;
+  return *this;
+}
+
+number_t number_t::operator&=(const number_t &right) {
+  this->x = int64_t(this->x) & int64_t(right.x);
+  return *this;
+}
+
+number_t operator|(number_t &left, const int64_t &right) {
+  return {int64_t(left.x) | right};
+}
+
+number_t operator|(const int64_t &left, number_t &right) {
+  return {left | int64_t(right.x)};
+}
+
+number_t operator|(const number_t &left, const number_t &right) {
+  return {int64_t(left.x) | int64_t(right.x)};
+}
+
+number_t number_t::operator|=(const int64_t &right) {
+  this->x = int64_t(this->x) | right;
+  return *this;
+}
+
+number_t number_t::operator|=(const number_t &right) {
+  this->x = int64_t(this->x) | int64_t(right.x);
+  return *this;
+}
+
+number_t number_t::operator~() const {
+  return {~(int64_t(this->x))};
+}
+
+number_t &number_t::operator<<=(const int64_t &right) {
+  x <<= right;
+  return *this;
+}
+
+number_t &number_t::operator>>=(const int64_t &right) {
+  x >>= right;
+  return *this;
+}
+
+number_t number_t::operator<<(const int64_t &right) const {
+  return {int64_t(this->x) << right};
+}
+
+number_t number_t::operator>>(const int64_t &right) const {
+  return {int64_t(this->x) >> right};
+}
--- number/number.h
+++ number/number.h
@@ -1,8 +1,139 @@
-#pragma once
-
 #include <cstdint>
+#include <string>
 #include <iostream>
 
 class number_t {
+private:
+  int64_t x;
+
+public:
+  number_t();
+  number_t(int64_t o_x);
+  number_t(double o_x);
+  number_t(int o_x);
+  number_t(const number_t &o_number_t);
+
+
+  explicit operator int() const;
+  explicit operator float() const;
+  explicit operator bool() const;
+  explicit operator int64_t() const;
+  explicit operator std::string() const;
+
+
+  number_t& operator=(const number_t &o_x);
+  number_t& operator=(const int64_t &o_x);
+
+
+
+  friend number_t operator+(number_t &val, const int64_t &val2);
+  friend number_t operator+(const int64_t &val, number_t &val2);
+  friend number_t operator+(const number_t& left, const number_t& right);
+
+  friend number_t operator-(const number_t &val, const int64_t &val2);
+  friend number_t operator-(const int64_t &val, const number_t &val2);
+  friend number_t operator-(const number_t& left, const number_t& right);
+
+  number_t & operator+=(const int64_t &val2);
+  number_t& operator+=(const number_t &val2);
+
+  number_t& operator-=(const int64_t &val2);
+  number_t& operator-=(const number_t &val2);
+
+  number_t operator-() const;
+  number_t operator+() const;
+
+
+
+  number_t& operator++();
+  number_t& operator--();
+  number_t operator++(int);
+  number_t operator--(int);
+
+
+  friend number_t operator*(const number_t &left, const int64_t & right);
+  friend number_t operator*(const int64_t &left, const number_t & right);
+  friend number_t operator*(const number_t& left, const number_t& right);
+
+  number_t operator*=(const int64_t & right);
+  number_t operator*=(const number_t& right);
+
+
+
+  friend number_t operator/(const number_t &left, const int64_t & right);
+  friend number_t operator/(const int64_t &left, const number_t & right);
+  friend number_t operator/(const number_t& left, const number_t& right);
+
+  number_t operator/=(const int64_t & right);
+  number_t operator/=(const number_t& right);
+
+
+  friend number_t operator%(number_t &left, const int64_t & right);
+  friend number_t operator%(const int64_t &left, number_t & right);
+  friend number_t operator%(const number_t& left, const number_t& right);
+
+  number_t operator%=(const int64_t & right);
+  number_t operator%=(const number_t& right);
+
+
+  friend number_t operator^(number_t &left, const int64_t & right);
+  friend number_t operator^(const int64_t &left, number_t & right);
+  friend number_t operator^(const number_t& left, const number_t& right);
+
+  number_t operator^=(const int64_t & right);
+  number_t operator^=(const number_t& right);
+
+
+  friend number_t operator&(number_t &left, const int64_t & right);
+  friend number_t operator&(const int64_t &left, number_t & right);
+  friend number_t operator&(const number_t& left, const number_t& right);
+
+  number_t operator&=(const int64_t & right);
+  number_t operator&=(const number_t& right);
+
+
+  friend number_t operator|(number_t &left, const int64_t & right);
+  friend number_t operator|(const int64_t &left, number_t & right);
+  friend number_t operator|(const number_t& left, const number_t& right);
+
+  number_t operator|=(const int64_t & right);
+  number_t operator|=(const number_t& right);
+
+
+  number_t operator~() const ;
+
+
+  number_t operator<<(const int64_t &right) const;
+
+  number_t operator>>(const int64_t &right) const;
+
+
+
+  number_t &operator <<=(const int64_t & right);
+
+  number_t &operator >>=(const int64_t & right);
+
+  bool operator ==(const number_t&val) const;
+  friend bool operator ==(const int64_t &val, const number_t&val2);
+  bool operator ==(const int64_t&val) const;
+  bool operator !=(const number_t& val) const;
+  bool operator !=(const int64_t& val) const;
+  friend bool operator !=(const int64_t&val, const number_t&val2);
+  bool operator >(const number_t& val) const;
+  bool operator >(const int64_t& val) const;
+  friend bool operator >(const int64_t&val, const number_t&val2);
+  bool operator <(const number_t& val) const;
+  bool operator <(const int64_t& val) const;
+  friend bool operator <(const int64_t& val, const number_t&val2);
+  bool operator >=(const number_t& val) const;
+  bool operator >=(const int64_t & val) const;
+  friend bool operator >=(const int64_t & val, const number_t& val2);
+  bool operator <=(const number_t& val) const;
+  friend bool operator <=(const int64_t& val, const number_t& val2);
+  bool operator <=(const int64_t & val) const;
+
+  friend std::ostream& operator<<(std::ostream& os, const number_t& number);
+  friend std::istream& operator>>(std::istream& is, number_t& number);
+
 
 };
